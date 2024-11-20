97402349dbfdbef60d11fdb27c18733b338b2683
Krtpai <112532849+Krtpai@users.noreply.github.com>
2024-11-20 09:57:57 +0800

    Update Delimiter and Brackets

--- krtlexer.c
+++ krtlexer.c
@@ -112,7 +112,7 @@ void processLine(char *line, int lineNumber, FILE *symbolTable) {
                 } else if (strchr("+-*/=%^~", c)) {
                     state = OPERATOR;
                     currentToken[i++] = c;
-                } else if (strchr("(){}[],;<>", c)) {
+                } else if (strchr("(){}[],;:'\".", c)) {
                     state = DELIMITER;
                     currentToken[i++] = c;
                 } else if (c == '~' && line[j + 1] == '~') {
@@ -189,80 +189,173 @@ void processLine(char *line, int lineNumber, FILE *symbolTable) {
                 break;
 
             case OPERATOR:
-                // Check for increment and decrement (++ and --)
-                if (currentToken[0] == '+') {
+                if (currentToken[0] == '=') {
+                    if (line[j] == '=') {
+                        // Equal To Operator (==)
+                        currentToken[i++] = line[j++]; // Append second '='
+                        currentToken[i] = '\0';       // Null-terminate
+                        writeToken(symbolTable, "Relational Operator (Equal To)", currentToken, lineNumber);
+                    } else {
+                        // Assignment Operator (=)
+                        currentToken[i] = '\0';
+                        writeToken(symbolTable, "Assignment Operator", currentToken, lineNumber);
+                    }
+                    i = 0;
+                    state = START;
+
+                } else if (currentToken[0] == '!') {
+                    if (line[j] == '=') {
+                        // Not Equal To Operator (!=)
+                        currentToken[i++] = line[j++]; // Append '='
+                        currentToken[i] = '\0';       // Null-terminate
+                        writeToken(symbolTable, "Relational Operator (Not Equal To)", currentToken, lineNumber);
+                    } else {
+                        // Unknown Operator (!)
+                        currentToken[i] = '\0';
+                        writeToken(symbolTable, "Unknown Operator", currentToken, lineNumber);
+                    }
+                    i = 0;
+                    state = START;
+
+                } else if (currentToken[0] == '>') {
+                    if (line[j] == '=') {
+                        // Greater Than or Equal To Operator (>=)
+                        currentToken[i++] = line[j++]; // Append '='
+                        currentToken[i] = '\0';       // Null-terminate
+                        writeToken(symbolTable, "Relational Operator (Greater Than or Equal To)", currentToken, lineNumber);
+                    } else {
+                        // Greater Than Operator (>)
+                        currentToken[i] = '\0';
+                        writeToken(symbolTable, "Relational Operator (Greater Than)", currentToken, lineNumber);
+                    }
+                    i = 0;
+                    state = START;
+
+                } else if (currentToken[0] == '<') {
+                    if (line[j] == '=') {
+                        // Less Than or Equal To Operator (<=)
+                        currentToken[i++] = line[j++]; // Append '='
+                        currentToken[i] = '\0';       // Null-terminate
+                        writeToken(symbolTable, "Relational Operator (Less Than or Equal To)", currentToken, lineNumber);
+                    } else {
+                        // Less Than Operator (<)
+                        currentToken[i] = '\0';
+                        writeToken(symbolTable, "Relational Operator (Less Than)", currentToken, lineNumber);
+                    }
+                    i = 0;
+                    state = START;
+
+                } else if (currentToken[0] == '+') {
                     if (line[j] == '+') { 
-                        // Increment operator
+                        // Increment Operator
                         currentToken[i++] = line[j++]; // Append second '+'
                         currentToken[i] = '\0';       // Null-terminate
-                        writeToken(symbolTable, "Unary Operator", currentToken, lineNumber);
-                        i = 0;
-                        state = START;
-                    } else {
-                        // Distinguish Unary Plus (+x) from Addition (x + y)
-                        currentToken[i] = '\0'; // Null-terminate
-                        if (isspace(line[j]) || isalpha(line[j]) || isdigit(line[j])) {
-                            // Space or operand after '+' => Arithmetic Operator
-                            writeToken(symbolTable, "Arithmetic Operator", currentToken, lineNumber);
-                        } else {
-                            // No operand => Unary Operator
-                            writeToken(symbolTable, "Unary Operator", currentToken, lineNumber);
-                        }
-                        i = 0;
-                        state = START;
-                    }
-                } else if (currentToken[0] == '-') {
-                    if (line[j] == '-') { 
-                        // Decrement operator
-                        currentToken[i++] = line[j++]; // Append second '-'
+                        writeToken(symbolTable, "Increment Operator", currentToken, lineNumber);
+                    } else if (line[j] == '=') {
+                        // Addition Assignment Operator (+=)
+                        currentToken[i++] = line[j++]; // Append '='
                         currentToken[i] = '\0';       // Null-terminate
-                        writeToken(symbolTable, "Unary Operator", currentToken, lineNumber);
-                        i = 0;
-                        state = START;
+                        writeToken(symbolTable, "Addition Assignment Operator", currentToken, lineNumber);
                     } else {
-                        // Distinguish Unary Minus (-x) from Subtraction (x - y)
-                        currentToken[i] = '\0'; // Null-terminate
-                        if (isspace(line[j]) || isalpha(line[j]) || isdigit(line[j])) {
-                            // Space or operand after '-' => Arithmetic Operator
-                            writeToken(symbolTable, "Arithmetic Operator", currentToken, lineNumber);
-                        } else {
-                            // No operand => Unary Operator
-                            writeToken(symbolTable, "Unary Operator", currentToken, lineNumber);
-                        }
-                        i = 0;
-                        state = START;
-                    }
-                } else if (currentToken[0] == '/') {
-                    if (line[j] == '/') {
-                        // Integer Division Operator (//)
-                        currentToken[i++] = line[j++]; // Append second '/'
-                        currentToken[i] = '\0';       // Null-terminate
-                        writeToken(symbolTable, "Arithmetic Operator", currentToken, lineNumber);
-                        i = 0;
-                        state = START;
-                    } else {
-                        // Regular Division Operator (/)
+                        // Addition Operator
                         currentToken[i] = '\0';
                         writeToken(symbolTable, "Arithmetic Operator", currentToken, lineNumber);
-                        i = 0;
-                        state = START;
                     }
-                } else if (currentToken[0] == '^') {
-                    // Exponentiation Operator (^)
-                    currentToken[i] = '\0';
-                    writeToken(symbolTable, "Arithmetic Operator", currentToken, lineNumber);
                     i = 0;
                     state = START;
-                } else if (strchr("*/%", currentToken[0])) {
-                    // Other arithmetic operators (*, /, %)
-                    currentToken[i] = '\0';
-                    writeToken(symbolTable, "Arithmetic Operator", currentToken, lineNumber);
+
+                } else if (currentToken[0] == '-') {
+                    if (line[j] == '-') { 
+                        // Decrement Operator
+                        currentToken[i++] = line[j++]; // Append second '-'
+                        currentToken[i] = '\0';       // Null-terminate
+                        writeToken(symbolTable, "Decrement Operator", currentToken, lineNumber);
+                    } else if (line[j] == '=') {
+                        // Subtraction Assignment Operator (-=)
+                        currentToken[i++] = line[j++]; // Append '='
+                        currentToken[i] = '\0';       // Null-terminate
+                        writeToken(symbolTable, "Subtraction Assignment Operator", currentToken, lineNumber);
+                    } else {
+                        // Subtraction Operator
+                        currentToken[i] = '\0';
+                        writeToken(symbolTable, "Arithmetic Operator", currentToken, lineNumber);
+                    }
                     i = 0;
                     state = START;
+
+                } else if (currentToken[0] == '*') {
+                    if (line[j] == '=') {
+                        // Multiplication Assignment Operator (*=)
+                        currentToken[i++] = line[j++]; // Append '='
+                        currentToken[i] = '\0';       // Null-terminate
+                        writeToken(symbolTable, "Multiplication Assignment Operator", currentToken, lineNumber);
+                    } else {
+                        // Multiplication Operator
+                        currentToken[i] = '\0';
+                        writeToken(symbolTable, "Arithmetic Operator", currentToken, lineNumber);
+                    }
+                    i = 0;
+                    state = START;
+
+                } else if (currentToken[0] == '/') {
+                    if (line[j] == '/') {
+                        if (line[j + 1] == '=') {
+                            // Integer Division Assignment Operator (//=)
+                            currentToken[i++] = line[j++]; // Append second '/'
+                            currentToken[i++] = line[j++]; // Append '='
+                            currentToken[i] = '\0';       // Null-terminate
+                            writeToken(symbolTable, "Integer Division Assignment Operator", currentToken, lineNumber);
+                        } else {
+                            // Integer Division Operator (//)
+                            currentToken[i++] = line[j++]; // Append second '/'
+                            currentToken[i] = '\0';       // Null-terminate
+                            writeToken(symbolTable, "Arithmetic Operator (Integer Division)", currentToken, lineNumber);
+                        }
+                    } else if (line[j] == '=') {
+                        // Division Assignment Operator (/=)
+                        currentToken[i++] = line[j++]; // Append '='
+                        currentToken[i] = '\0';       // Null-terminate
+                        writeToken(symbolTable, "Division Assignment Operator", currentToken, lineNumber);
+                    } else {
+                        // Division Operator (/)
+                        currentToken[i] = '\0';
+                        writeToken(symbolTable, "Arithmetic Operator", currentToken, lineNumber);
+                    }
+                    i = 0;
+                    state = START;
+
+                } else if (currentToken[0] == '%') {
+                    if (line[j] == '=') {
+                        // Modulo Assignment Operator (%=)
+                        currentToken[i++] = line[j++]; // Append '='
+                        currentToken[i] = '\0';       // Null-terminate
+                        writeToken(symbolTable, "Modulo Assignment Operator", currentToken, lineNumber);
+                    } else {
+                        // Modulo Operator
+                        currentToken[i] = '\0';
+                        writeToken(symbolTable, "Arithmetic Operator", currentToken, lineNumber);
+                    }
+                    i = 0;
+                    state = START;
+
+                } else if (currentToken[0] == '~') {
+                    if (line[j] == '=') {
+                        // Bitwise NOT Assignment Operator (~=)
+                        currentToken[i++] = line[j++]; // Append '='
+                        currentToken[i] = '\0';       // Null-terminate
+                        writeToken(symbolTable, "Bitwise NOT Assignment Operator", currentToken, lineNumber);
+                    } else {
+                        // Bitwise NOT Operator
+                        currentToken[i] = '\0';
+                        writeToken(symbolTable, "Bitwise NOT Operator", currentToken, lineNumber);
+                    }
+                    i = 0;
+                    state = START;
+
                 } else {
                     // Default case: Handle unknown operators
                     currentToken[i] = '\0';
-                    writeToken(symbolTable, "Unknown", currentToken, lineNumber);
+                    writeToken(symbolTable, "Unknown Operator", currentToken, lineNumber);
                     i = 0;
                     state = START;
                 }
